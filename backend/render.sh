09088b5e31d4c3e3834d70cec497454c46d9b4f4
JuanSebastianBorja <borja.juan@correounivalle.edu.co>
2026-06-01 12:22:33 -0500

    crear super usuario

--- backend/render.sh
+++ backend/render.sh
@@ -3,7 +3,16 @@
 echo "Iniciando migraciones..."
 python manage.py migrate --noinput
 
-echo "Migraciones completadas."
+echo "Creando superusuario por defecto si no existe..."
+# Crea un usuario admin 
+python manage.py shell << EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'admin123')
+    print("Superusuario 'admin' creado con éxito.")
+else:
+    print("El superusuario 'admin' ya existe.")
+EOF
 
 echo "Iniciando servidor..."
-exec gunicorn config.wsgi:application --bind 0.0.0.0:$PORT
